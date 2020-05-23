7ff198a3ac616f91c5ce4641ddb3df5425b7b232
marlenetest <65796557+marlenetest@users.noreply.github.com>
2020-05-23 03:35:03 +0200

    Add files via upload

---
+++ Martine.js
@@ -0,0 +1,13 @@
+// get video element id
+var vidClip = document.getElementById("martineVideo1"); 
+
+// play video event
+function playVid() { 
+    martineVideo1.play();
+} 
+
+// pause video event
+function pauseVid() { 
+  martineVideo1.pause(); 
+}
+
